3cc9effb1b982683ceb8de961a33127ff8e46515
tristan berkmans <r0784105@student.thomasmore.be>
2023-12-31 15:37:35 +0100

    change to mysql database v2

--- app/database.py
+++ app/database.py
@@ -3,32 +3,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# #connecteren met de database
+#connecteren met de database
+import os
+
+# Read the DATABASE_URL environment variable
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sqlitedb/sqlitedata1.db")
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlitedb/sqlitedata1.db"
-# # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-# #qqnmqken
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
-
-# Connection details for MySQL
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://{user}:{password}@mysql/{db}"
-
-# Create the engine
+#qqnmqken
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL.format(
-        user="user",
-        password="4321Secret1234",
-        db="data",
-    ),
-    pool_pre_ping=True,
+    DATABASE_URL, connect_args={"check_same_thread": False}
 )
-
-
-
-
-
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
